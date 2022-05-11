09c3d94dd0a6cdd4dc7f8db53e82bf7678f1e370
EGoesche <e.goesche@outlook.de>
2022-05-12 00:51:03 +0200

    Add show method

--- ex01/main.py
+++ ex01/main.py
@@ -1,4 +1,8 @@
 from pattern import *
+from generator import *
 
-c1 = Circle(1024, 200, (512, 456))
-c1.show()
+# c1 = Circle(1024, 200, (512, 456))
+# c1.show()
+
+g1 = ImageGenerator('./data/exercise_data/', './data/Labels.json', 10, (100, 100))
+g1.show()
