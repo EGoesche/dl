90ee2b5a99a822532a6c707ba6727d9409bd7b09
EGoesche <e.goesche@outlook.de>
2022-05-23 12:20:29 +0200

    Create ReLU.py

---
+++ ex01/exercise1_material/src_to_implement/Layers/ReLU.py
@@ -0,0 +1,14 @@
+import Base
+
+
+class ReLU(Base):
+    def __init__(self):
+        pass
+
+    def forward(input_tensor):
+        # TODO: Implement the forward method
+        pass
+
+    def backward(error_tensor):
+        # TODO: Implement the backward method
+        pass
