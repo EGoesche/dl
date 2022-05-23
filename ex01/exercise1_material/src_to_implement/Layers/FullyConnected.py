3db15b297a6495fe2f60c0c8dde1964813e466b6
EGoesche <e.goesche@outlook.de>
2022-05-23 12:20:21 +0200

    Add forward and backward partly

--- ex01/exercise1_material/src_to_implement/Layers/FullyConnected.py
+++ ex01/exercise1_material/src_to_implement/Layers/FullyConnected.py
@@ -1,4 +1,7 @@
+import random
+
 import Base
+import numpy as np
 
 
 class FullyConnected(Base):
@@ -6,17 +9,24 @@ class FullyConnected(Base):
         super().trainable = True
         self.input_size = input_size
         self.output_size = output_size
+        self.input_tensor = None
         self._optimizer = None
-        self.weights = None
+        self.weights = [random.uniform(0, 1) for _ in range(input_size)]
         self._gradient_weights = None
 
-    def forward(input_tensor):
-        # TODO: Implement the forward method
-        pass
+    def forward(self, input_tensor):
+        self.input_tensor = input_tensor    # create a copy for backward pass
+        return np.matmul(input_tensor, self.weights)
 
-    def backward(error_tensor):
-        # TODO: Implement the backward method
-        pass
+    def backward(self, error_tensor):
+        # Calculate gradient
+        self._gradient_weights = np.matmul(self.input_tensor.T, error_tensor)
+
+        # Update weights
+        if self._optimizer is not None:
+            self.weights = self._optimizer.calculate_update(self.weights, self._gradient_weights)
+
+        # TODO: return error tensor for prev layer
 
     @property
     def optimizer(self):
