28f2bfdeb050860b5655f5aea883788e5a10ea70
summersonnn <kyazogluu@gmail.com>
2022-05-24 20:39:37 +0200

    Class definition for Sgd is added

--- ex01/exercise1_material/src_to_implement/Optimization/Optimizers.py
+++ ex01/exercise1_material/src_to_implement/Optimization/Optimizers.py
@@ -1,6 +1,7 @@
-def __init__(self, learning_rate):
-    self.learning_rate = learning_rate
+class Sgd:
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
 
 
-def calculate_update(self, weight_tensor, gradient_tensor):
-    return weight_tensor - self.learning_rate * gradient_tensor
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        return weight_tensor - self.learning_rate * gradient_tensor
