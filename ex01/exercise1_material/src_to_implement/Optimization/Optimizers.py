056177f04a53fff4cb2e3b64971cccc12a6fb421
summersonnn <kyazogluu@gmail.com>
2022-05-24 20:40:46 +0200

    same comment as one before, just deleted blank lines

--- ex01/exercise1_material/src_to_implement/Optimization/Optimizers.py
+++ ex01/exercise1_material/src_to_implement/Optimization/Optimizers.py
@@ -2,6 +2,5 @@ class Sgd:
     def __init__(self, learning_rate):
         self.learning_rate = learning_rate
 
-
     def calculate_update(self, weight_tensor, gradient_tensor):
         return weight_tensor - self.learning_rate * gradient_tensor
