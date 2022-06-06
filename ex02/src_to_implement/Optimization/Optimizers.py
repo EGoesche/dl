d9ec73b0880fc2d0bf24f071dad709063b9371a9
summersonnn <kyazogluu@gmail.com>
2022-06-06 12:42:51 +0200

    Optimizers is done. Check the class variables

--- ex02/src_to_implement/Optimization/Optimizers.py
+++ ex02/src_to_implement/Optimization/Optimizers.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 class Sgd:
     def __init__(self, learning_rate):
         """
@@ -14,3 +17,37 @@ class Sgd:
         :return: updated weight tensor
         """
         return weight_tensor - self.learning_rate * gradient_tensor
+
+class SgdWithMomentum:
+    def __init__(self, learning_rate, momentum_rate):
+        self.learning_rate = learning_rate
+        self.momentum_rate = momentum_rate
+        self.change = 0   #Stores the direction we're going. Basically w_(t) - w_(t-1)
+
+
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        self.change = self.momentum_rate * self.change - self.learning_rate * gradient_tensor
+        return weight_tensor + self.change
+
+
+
+class Adam:
+    def __init__(self, learning_rate, mu, rho):
+        self.learning_rate = learning_rate
+        self.mu = mu
+        self.rho = rho
+        self.change = 0
+        self.r_k = 0
+        self.iter = 0
+
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        self.change = self.mu * self.change + (1-self.mu) * gradient_tensor
+        self.r_k = self.rho * self.r_k + np.dot((1-self.rho)*gradient_tensor, gradient_tensor)
+
+        #Bias correction
+        self.change = self.change / (1 - self.mu**self.iter)
+        self.r_k = self.r_k / (1 - self.rho**self.iter)
+
+        self.iter += 1
+
+        return weight_tensor - self.learning_rate * self.change / (math.sqrt(self.r_k) + np.finfo(float).eps)
