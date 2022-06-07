d7960079615f77ffd90b8619ec6bea3f879e71cc
EGoesche <e.goesche@outlook.de>
2022-06-07 11:19:30 +0200

    Reformatting in flatten and add todo

--- ex02/src_to_implement/Layers/Flatten.py
+++ ex02/src_to_implement/Layers/Flatten.py
@@ -1,14 +1,16 @@
 import numpy as np
 
+
 class Flatten:
     def __init__(self):
         self.shape = None
 
-    def forward(self,input_tensor):
-        self.shape = input_tensor[0].shape  #Store this so that we'll know the output shape when doing backward()
+    def forward(self, input_tensor):
+        self.shape = input_tensor[0].shape  # Store this so that we'll know the output shape when doing backward()
         batches = len(input_tensor)
         flatten_outputs = []
 
+        # TODO: if there is enough time, we should try to kick out the loop
         for batch in range(0, batches):
             flatten_outputs.append(np.reshape(input_tensor[batch], -1))
         flatten_outputs = np.array(flatten_outputs)
