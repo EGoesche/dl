29ec142d03c6ab042735945b3fd6c36fd1101557
EGoesche <e.goesche@outlook.de>
2022-06-15 18:36:40 +0200

    Add forward pass to conv layer
    Not tested.

--- ex02/src_to_implement/Layers/Conv.py
+++ ex02/src_to_implement/Layers/Conv.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import signal
 
 from src_to_implement.Layers import Base
 
@@ -7,7 +8,6 @@ class Conv(Base.BaseLayer):
     def __init__(self, stride_shape, convolution_shape, num_kernels):
         super().__init__()
         self.trainable = True
-        self._optimizer = None  # TODO: Maybe we need two optimizer (weights + bias)
         self.stride_shape = stride_shape  # allows for different strides in the spatial dimensions
         self.convolution_shape = convolution_shape  # determines whether this object provides a 1D or a 2D conv layer
         self.num_kernels = num_kernels
@@ -19,6 +19,7 @@ class Conv(Base.BaseLayer):
         self._gradient_weights = np.zeros(self.weights.shape)
         self._gradient_bias = np.zeros(self.bias.shape)
         self.input_tensor = None
+        self._optimizer = None  # TODO: Maybe we need two optimizer (weights + bias)
 
     @property
     def gradient_weights(self):
@@ -46,7 +47,32 @@ class Conv(Base.BaseLayer):
 
     def forward(self, input_tensor):
         self.input_tensor = input_tensor  # create a copy for backward pass
-        # TODO: Implement forward pass
+        feature_maps = []  # feature maps that form the output of the forward pass
+        for channel in self.input_tensor:
+            channel_feature_map = []  # feature maps in the current channel
+            for num_kernel in range(self.num_kernels):
+                # Do a convolution of current image (or 1D signal) with current kernel
+                conv_channel = signal.correlate(channel, self.weights[num_kernel], 'same')
+
+                # extract the valid (middle) channel (number of input channels // 2)
+                conv_channel = conv_channel[self.convolution_shape[0] // 2]
+
+                # element-wise addition of bias which belongs to the current kernel
+                conv_channel += self.bias[num_kernel]
+
+                # stride
+                if len(self.stride_shape) == 1:
+                    # In case of 1D signals
+                    conv_channel = conv_channel[::self.stride_shape[0]]
+                else:
+                    # In case of images
+                    conv_channel = conv_channel[::self.stride_shape[0], ::self.stride_shape[1]]
+
+                channel_feature_map.append(conv_channel)
+            feature_maps.append(channel_feature_map)
+            output = np.array(feature_maps)
+
+            return output
 
     def backward(self, error_tensor):
         # TODO: Implement backward pass
