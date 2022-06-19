ccc95a28ebd46e9020eee6f13a8fdbef32e1ac5e
summersonnn <kyazogluu@gmail.com>
2022-06-19 18:51:36 +0200

    bug fix in Adam, test passes now

--- ex02/src_to_implement/Optimization/Optimizers.py
+++ ex02/src_to_implement/Optimization/Optimizers.py
@@ -36,18 +36,18 @@ class Adam:
         self.learning_rate = learning_rate
         self.mu = mu
         self.rho = rho
-        self.change = 0
+        self.v_k = 0
         self.r_k = 0
-        self.iter = 0
+        self.epsilon = 1e-8
+        self.iter = 1
 
     def calculate_update(self, weight_tensor, gradient_tensor):
-        self.change = self.mu * self.change + (1 - self.mu) * gradient_tensor
-        self.r_k = self.rho * self.r_k + np.dot((1 - self.rho) * gradient_tensor, gradient_tensor)
+        self.v_k = self.mu * self.v_k + (1 - self.mu) * gradient_tensor
+        self.r_k = self.rho * self.r_k + (1 - self.rho) * (gradient_tensor ** 2)
 
         # Bias correction
-        self.change = self.change / (1 - self.mu ** self.iter)
-        self.r_k = self.r_k / (1 - self.rho ** self.iter)
-
+        v_k_corr = self.v_k / (1 - self.mu ** self.iter)
+        r_k_corr = self.r_k / (1 - self.rho ** self.iter)
         self.iter += 1
 
-        return weight_tensor - self.learning_rate * self.change / (math.sqrt(self.r_k) + np.finfo(float).eps)
+        return weight_tensor - self.learning_rate * (v_k_corr / (np.sqrt(r_k_corr) + self.epsilon))
