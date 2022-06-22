gradient wrt weights fix - incomplete

ex02/src_to_implement/Layers/Conv.py
@@ -130,14 +130,11 @@ class Conv(Base.BaseLayer):
         # Gradient with respect to weights
         # QUESTION: Here, the correlation operation is done for every image in the batch and the results r concatenated
         # The following is not correct due to error_tensor being a batch, not a single input. FIX!
-        '''
-        gradient_wrt_weights = []
         for image in self.input_tensor:
             for channel in range(self.input_tensor.shape[1]):
                 corr_channel = signal.correlate(image[channel], error_tensor[channel], 'same')
                 corr_channel = corr_channel[self.convolution_shape[0] // 2]
-                gradient_wrt_weights.append(corr_channel)
-        gradient_wrt_weights = np.array(gradient_wrt_weights) '''
+                self.gradient_weights = np.append(self.gradient_weights, corr_channel)
 
         # Update weights and bias
         if self._optimizer is not None:

ex02/src_to_implement/NeuralNetworkTests.py
@@ -532,7 +532,7 @@ class TestConv(unittest.TestCase):
 
     def test_1D_forward_size(self):
         conv = Conv.Conv([2], (3, 3), self.num_kernels)
-        input_tensor = np.array(range(3 * 15 * self.batch_size), dtype=np.float)
+        input_tensor = np.array(range(3 * 15 * self.batch_size), dtype=float)
         input_tensor = input_tensor.reshape((self.batch_size, 3, 15))
         output_tensor = conv.forward(input_tensor)
         self.assertEqual(output_tensor.shape,  (self.batch_size,self.num_kernels, 8))
@@ -586,6 +586,7 @@ class TestConv(unittest.TestCase):
         layers.append(Flatten.Flatten())
         layers.append(L2Loss())
         difference = Helpers.gradient_check(layers, input_tensor, self.label_tensor)
+        print(self.label_tensor.shape)
         self.assertLessEqual(np.sum(difference), 5e-2)
 
     def test_gradient_weights(self):