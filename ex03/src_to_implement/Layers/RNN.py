266600bc73591c3c098b7b410b362a2ecd8a27e9
summersonnn <kyazogluu@gmail.com>
2022-07-10 19:30:43 +0200

    Initialization and first lines of codes for RNN class

--- ex03/src_to_implement/Layers/RNN.py
+++ ex03/src_to_implement/Layers/RNN.py
@@ -1,5 +1,6 @@
 import numpy as np
 from Layers import Base
+import copy
 
 
 class RNN(Base.BaseLayer):
@@ -8,10 +9,61 @@ class RNN(Base.BaseLayer):
         # hidden_size = dimension of the hidden state
         super().__init__()
         self.trainable = True
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.hidden_state = np.zeros(hidden_size)
+        self._memorize = False
+
+        self._gradient_weights = np.zeros(self.weights.shape)
+        self._weights = None # This is probably wrong, come back here
+        self._weights_optimizer = None
+        self._bias_optimizer = None
+
+    @property
+    def memorize(self):
+        return self._memorize
+
+    @memorize.setter
+    def memorize(self, memorize):
+        self._memorize = memorize
+
+    @property
+    def weights(self):
+        return self._weights
+
+    @weights.setter
+    def weights(self, weights):
+        self._weights = weights
+
+    @property
+    def gradient_weights(self):
+        return self._gradient_weights
+
+    @gradient_weights.setter
+    def gradient_weights(self, weights):
+        self._gradient_weights = weights
+
+    @property
+    def optimizer(self):
+        return self._weights_optimizer, self._bias_optimizer
+
+    @optimizer.setter
+    def optimizer(self, optimizer):
+        self._weights_optimizer = copy.deepcopy(optimizer)
+        self._bias_optimizer = copy.deepcopy(optimizer)
+
 
     def forward(self, input_tensor):
         pass
 
     def backward(self, error_tensor):
         pass
+
+    def initialize(self, weights_initializer, bias_initializer):
+        pass
+
+    def calculate_regularization_loss(self):
+        pass
+
 
