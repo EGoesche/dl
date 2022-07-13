681675b0349f8c2a805eed9e9b12238f64f6dc46
EGoesche <e.goesche@outlook.de>
2022-07-13 22:36:55 +0200

    Add forward pass in RNN

--- ex03/src_to_implement/Layers/RNN.py
+++ ex03/src_to_implement/Layers/RNN.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Layers import Base, FullyConnected
+from Layers import Base, FullyConnected, TanH, Sigmoid
 import copy
 
 
@@ -8,6 +8,7 @@ class RNN(Base.BaseLayer):
         # input_size = dimension of the input vector
         # hidden_size = dimension of the hidden state
         super().__init__()
+
         self.trainable = True
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -15,12 +16,11 @@ class RNN(Base.BaseLayer):
         self.hidden_state = np.zeros(hidden_size)
         self._memorize = False
 
-        self._gradient_weights = np.zeros(self.weights.shape)
-
         self.fc_hidden = FullyConnected.FullyConnected(self.input_size + self.hidden_size, self.hidden_size)
         self.fc_output = FullyConnected.FullyConnected(self.hidden_size, self.output_size)
         self._weights = self.fc_hidden.weights
         self._weights_output = self.fc_output.weights
+        self._gradient_weights = np.zeros(self.weights.shape)
 
         self._weights_optimizer = None
         self._bias_optimizer = None
@@ -67,15 +67,35 @@ class RNN(Base.BaseLayer):
         self._bias_optimizer = copy.deepcopy(optimizer)
 
     def forward(self, input_tensor):
-        pass
+        output = None
 
-    def backward(self, error_tensor):
-        pass
+        if not self.memorize:
+            self.hidden_state = np.zeros(self.hidden_size)
 
-    def initialize(self, weights_initializer, bias_initializer):
-        pass
+        for time in range(len(input_tensor)):
+            input_hidden_concatenated = np.array([np.append(input_tensor[time], self.hidden_state)])
+            input_hidden_concatenated = self.fc_hidden.forward(input_hidden_concatenated)
 
-    def calculate_regularization_loss(self):
-        pass
+            self.hidden_state = TanH.TanH().forward(input_hidden_concatenated)
+
+            dot_product_hidden_state_and_weights_fc_output = self.fc_output.forward(self.hidden_state)
+            current_output = Sigmoid.Sigmoid().forward(dot_product_hidden_state_and_weights_fc_output)
+
+            if output is not None:
+                output = np.concatenate((output, current_output))
+            else:
+                output = current_output
+
+        return output
 
 
+def backward(self, error_tensor):
+    pass
+
+
+def initialize(self, weights_initializer, bias_initializer):
+    pass
+
+
+def calculate_regularization_loss(self):
+    pass
