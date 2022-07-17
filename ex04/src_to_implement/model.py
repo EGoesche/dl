af6f83247464988b98a302812cc9483d93bed0c4
summersonnn <kyazogluu@gmail.com>
2022-07-17 13:01:56 +0200

    ResNet is implemented without backward pass, not tested

--- ex04/src_to_implement/model.py
+++ ex04/src_to_implement/model.py
@@ -1,11 +1,35 @@
 import torch.nn as nn
 
 class ResNet(nn.Module):
-    def __init__(self):
+    def __init__(self, resblock):
         super().__init__()
         self.training = True
 
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2),  #3 = input channels  64 = output channels
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
 
+        self.layer1 = nn.Sequential(
+            resblock(64, 64, stride=1),
+            resblock(64, 128, stride=2),
+            resblock(128, 256, stride=2),
+            resblock(256, 512, stride=2),
+        )
+
+        self.gap = nn.AdaptiveAvgPool2d(1)  # may be wrong
+        self.fc = nn.Linear(512, 2)
+
+    def forward(self, input):
+        layer0_output   = self.layer0(input)
+        layer1_output   = self.layer1(layer0_output)
+        gap_output      = self.gap(input)
+        fl_output       = nn.flatten(gap_output)
+        fc_output       = self.fc(fl_output)
+
+        return nn.Sigmoid(fc_output)
 
 
 class ResBlock(nn.Module):
